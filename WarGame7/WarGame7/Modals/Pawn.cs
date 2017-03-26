4d89f9547d0d0050cd70fc45cd207a9edb2441b6
Freeware Sys <Freeware Sys@ALEXATASHA>
2017-03-26 14:01:23 +0300

    pawn class created (incomplete)

---
+++ WarGame7/WarGame7/Modals/Pawn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame7
+{
+    public class Pawn
+    {
+        public int Position { get; set; }
+
+    }
+}
